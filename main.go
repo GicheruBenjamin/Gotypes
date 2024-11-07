bcee415ea17acb70fdf99b8282b836204869fc4e
GicheruBenjamin <benjaminnjuguna77@gmail.com>
2024-11-07 04:01:22 +0300

    feature: Int functions and operations done

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"gotypes/src"
 )
 
 
 func main() {
-    str.Str()
+
+	//Chose the type
+	fmt.Println("Choose the type you want to demonstrate:")
+    fmt.Println("1. Integer")
+    fmt.Println("3. String")
+    fmt.Print("Enter your choice: ")
+
+    var choice int
+    fmt.Scanln(&choice)
+    switch choice {
+    case 1:
+        src.IntOperations()
+    case 3:
+		src.Str()
+	default:
+		fmt.Println("Invalid choice!")
+		break
+	}
 }
--- src/str.go
+++ src/str.go
@@ -1,5 +1,5 @@
-// src/str.go
-package str
+
+package src
 
 import (
 	"fmt"
