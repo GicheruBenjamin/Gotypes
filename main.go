0c2785f99b3fdf9d637ec640c02a0a111129996d
GicheruBenjamin <benjaminnjuguna77@gmail.com>
2024-11-13 16:27:36 +0300

    bug-fix: Removed the package causing errors

--- main.go
+++ main.go
@@ -2,27 +2,13 @@ package main
 
 import (
 	"fmt"
-	"gotypes/src"
 )
 
 
 func main() {
 
-	//Chose the type
-	fmt.Println("Choose the type you want to demonstrate:")
-    fmt.Println("1. Integer")
-    fmt.Println("3. String")
-    fmt.Print("Enter your choice: ")
+	//Welcome to the app
+	fmt.Println("Welcome to the app")
 
-    var choice int
-    fmt.Scanln(&choice)
-    switch choice {
-    case 1:
-        src.IntOperations()
-    case 3:
-		src.Str()
-	default:
-		fmt.Println("Invalid choice!")
-		break
-	}
+
 }
